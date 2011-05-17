8106d2a66a6f8196cce85697728d383394fd2b51
sacrac <carcas@sacrac.info>
2011-05-17 17:26:24 -0600

    agregando todos los grafos de mcas

--- encuesta/views.py
+++ encuesta/views.py
@@ -122,11 +122,423 @@ def familia_miembros(request):
     grafo_url = grafos.make_graph(valores, leyenda, '¿Miembros de la familia?', type=grafos.PIE_CHART_3D, size=(958, 313), pie_labels=True)
     return render_to_response('encuesta/familia/miembros.html', RequestContext(request, locals()))
 
-def conocimiento_abuso(request):
-    for abuso in Abuso.objects.all():
-        pass
-    pass
+#SALIDAS DE CONOCIMIENTOS
 
+def conocimiento_abuso(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for abuso in Abuso.objects.all():
+        suma = Conocimiento.objects.filter(encuesta__in=encuestas, abuso=abuso).count()
+        dicc[abuso.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/conocimiento/abuso.html', RequestContext(request, locals()))
+
+def conocimiento_lugar(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for lugar in LugarAbuso.objects.all():
+        suma = Conocimiento.objects.filter(encuesta__in=encuestas, lugares=lugar).count()
+        dicc[lugar.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/conocimiento/lugar.html', RequestContext(request, locals()))
+
+def conocimiento_abusan_ninos(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for abuso in Abusador.objects.all():
+        suma = Conocimiento.objects.filter(encuesta__in=encuestas, quien_abusa=abuso).count()
+        dicc[abuso.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/conocimiento/abusan_ninos.html', RequestContext(request, locals()))
+    
+def conocimiento_prevenir(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for hacer in QueHacer.objects.all():
+        suma = Conocimiento.objects.filter(encuesta__in=encuestas, que_hacer=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/conocimiento/prevenir.html', RequestContext(request, locals()))
+    
+
+def conocimiento_leyes(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in SI_NO:
+        suma = Conocimiento.objects.filter(encuesta__in=encuestas, conoce_ley=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Conoce sobre las leyes que castigan a las personas que abusan?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/conocimiento/leyes.html', RequestContext(request, locals()))
+    
+def conocimiento_aprendio(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for aprender in DondeAprendio.objects.all():
+        suma = Conocimiento.objects.filter(encuesta__in=encuestas, donde_aprendio=aprender).count()
+        dicc[aprender.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/conocimiento/aprendio.html', RequestContext(request, locals()))   
+    
+def conocimiento_informarse(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for informar in DondeInformarse.objects.all():
+        suma = Conocimiento.objects.filter(encuesta__in=encuestas, donde_informarse=informar).count()
+        dicc[informar.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/conocimiento/informarse.html', RequestContext(request, locals()))     
+
+#SALIDAS DE ACTITUD
+
+def actitud_abuso(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for abuso in PorqueAbuso.objects.all():
+        suma = Actitud.objects.filter(encuesta__in=encuestas, porque_abuso=abuso).count()
+        dicc[abuso.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/actitud/abuso.html', RequestContext(request, locals()))   
+
+def actitud_piensa(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for pensar in QuePiensa.objects.all():
+        suma = Actitud.objects.filter(encuesta__in=encuestas, que_piensa=pensar).count()
+        dicc[pensar.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/actitud/piensa.html', RequestContext(request, locals()))
+    
+    
+def actitud_victimas(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    dicc = {}
+    for victima in QuePiensaVictima.objects.all():
+        suma = Actitud.objects.filter(encuesta__in=encuestas, que_piensa_victimas=victima).count()
+        dicc[victima.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/actitud/victima.html', RequestContext(request, locals()))
+    
+def actitud_familia(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in SI_NO:
+        suma = Actitud.objects.filter(encuesta__in=encuestas, familia_ensena=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Estaría usted de acuerdo que en la Familia se enseñe a los niños, niñas y adolescentes a prevenir el abuso sexual?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/actitud/familia.html', RequestContext(request, locals()))  
+    
+def actitud_escuela(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in SI_NO:
+        suma = Actitud.objects.filter(encuesta__in=encuestas, escuela_ensena=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Estaría usted de acuerdo que en la Escuela se enseñe a los niños, niñas y adolescentes a prevenir el abuso sexual?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/actitud/escuela.html', RequestContext(request, locals()))
+    
+#PRACTICA
+
+def practica_situacion(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in QueHaria.objects.all():
+        suma = Practica.objects.filter(encuesta__in=encuestas, que_haria=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/practica/situacion.html', RequestContext(request, locals()))  
+    
+def practica_prevenir(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in QueHacePrevenir.objects.all():
+        suma = Practica.objects.filter(encuesta__in=encuestas, que_hago_prevenir=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/practica/prevenir.html', RequestContext(request, locals()))  
+    
+def practica_participa_prevenir(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in SI_NO:
+        suma = Practica.objects.filter(encuesta__in=encuestas, participa_prevenir=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Usted participa o ha participado en algún tipo de organización que previene el abuso sexual?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/practica/participa_prevenir.html', RequestContext(request, locals()))
+    
+def practica_como(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in ComoParticipo.objects.all():
+        suma = Practica.objects.filter(encuesta__in=encuestas, como=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/practica/como.html', RequestContext(request, locals())) 
+    
+#SALIDAS DE ESTADO ACTUAL
+
+def estado_problema(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in PROBLEMA:
+        suma = EstadoActual.objects.filter(encuesta__in=encuestas, problema_comunidad=hacer[0]).count()
+        dicc[hacer[1]] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/estado/problema_comunidad.html', RequestContext(request, locals())) 
+    
+def estado_problema_pais(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in PROBLEMA:
+        suma = EstadoActual.objects.filter(encuesta__in=encuestas, problema_pais=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Usted considera que el abuso sexual es un problema en el país?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/estado/problema_pais.html', RequestContext(request, locals()))
+    
+def estado_atencion(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in SI_NO:
+        suma = EstadoActual.objects.filter(encuesta__in=encuestas, personas_atienden=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Sabe si hay lugares o personas en esta comunidad que atienden a niños/as, adolescentes que vivieron abuso sexual?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/estado/atencion.html', RequestContext(request, locals()))
+    
+def estado_lugares(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in LugarAtencion.objects.all():
+        suma = EstadoActual.objects.filter(encuesta__in=encuestas, lugares=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/estado/lugares.html', RequestContext(request, locals())) 
+    
+def estado_tipo_atencion(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in TipoAtencion.objects.all():
+        suma = EstadoActual.objects.filter(encuesta__in=encuestas, tipo_atencion=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/estado/tipo_atencion.html', RequestContext(request, locals())) 
+    
+def estado_donde(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in DONDE_VAN:
+        suma = EstadoActual.objects.filter(encuesta__in=encuestas, donde_van=hacer[0]).count()
+        dicc[hacer[1]] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/estado/donde.html', RequestContext(request, locals())) 
+    
+# SALIDAS DE PERCEPCION
+
+def percepcion_ninos_abusados(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in SI_NO:
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, conoce_abusados=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Ha conocido de niños, niñas o adolescentes que hayan sido abusados sexualmente?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/percepcion/ninos_abusados.html', RequestContext(request, locals())) 
+    
+def percepcion_familia(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in QueFamilia.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, que_familia=opcion).count()
+        valores.append(suma)
+        leyenda.append(opcion.nombre)        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿En cuál de las siguientes familias se da más frecuentemente el abuso sexual?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/percepcion/familia_abuso.html', RequestContext(request, locals())) 
+    
+def percepcion_prevenir(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in QuienDebe.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, quien_debe=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/percepcion/encargado_prevenir.html', RequestContext(request, locals())) 
+    
+def percepcion_mensajes(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in MensajeTransmiten.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, mensaje=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/percepcion/mensajes.html', RequestContext(request, locals()))
+    
+def percepcion_reducir(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in SI_NO_SABE:
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, defender=opcion[0]).count()
+        valores.append(suma)
+        leyenda.append(opcion[1])        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Cree usted que si la población se organizará para defender los derechos humanos, de niños/as y adolescentes, el abuso se disminuiría?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/percepcion/reducir.html', RequestContext(request, locals())) 
+    
+def percepcion_iglesia(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in RolIglesia.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, rol_iglesia=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/percepcion/iglesia.html', RequestContext(request, locals())) 
+    
+def percepcion_medios(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()
+    valores = []
+    leyenda = []    
+    for opcion in RolMedio.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, rol_medios=opcion).count()
+        valores.append(suma)
+        leyenda.append(opcion)        
+
+    grafo_url = grafos.make_graph(valores, leyenda, '¿Qué piensa sobre el rol de los medios de comunicación en cuanto al abuso sexual?', type=grafos.PIE_CHART_3D, pie_labels=True)
+
+    return render_to_response('encuesta/percepcion/medios.html', RequestContext(request, locals())) 
+    
+def percepcion_estado(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in RolEstado.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, rol_estado=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/percepcion/estado.html', RequestContext(request, locals()))
+    
+def percepcion_ong(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in RolOng.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, rol_ongs=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/percepcion/ongs.html', RequestContext(request, locals()))
+    
+def percepcion_empresa(request):
+    encuestas = _query_set_filtrado(request)
+    numero = encuestas.count()    
+    dicc = {}
+    for hacer in RolEmpresa.objects.all():
+        suma = Percepcion.objects.filter(encuesta__in=encuestas, rol_empresas=hacer).count()
+        dicc[hacer.nombre] = suma
+    
+    dicc2 = sorted(dicc.items(), key=lambda x: x[1], reverse=True)
+    
+    return render_to_response('encuesta/percepcion/ongs.html', RequestContext(request, locals()))     
+    
+#FUNCIONES UTILITARIAS
 
 def get_munis(request):
     '''Metodo para obtener los municipios via Ajax segun los departamentos selectos'''
@@ -173,6 +585,43 @@ VALID_VIEWS = {
     'miembros': familia_miembros,
     #vistas para conocimiento
     'que-es-abuso': conocimiento_abuso,
+    'en-que-lugar': conocimiento_lugar,
+    'quines-abusan': conocimiento_abusan_ninos,
+    'como-prevenir': conocimiento_prevenir,
+    'leyes': conocimiento_leyes,
+    'donde-aprendio': conocimiento_aprendio,
+    'donde-informarse': conocimiento_informarse,
+    #vistas para actitud
+    'porque-abuso': actitud_abuso,
+    'abusadores': actitud_piensa,
+    'victimas': actitud_victimas,
+    'familia-ensena-ninos': actitud_familia,
+    'escuela-ensena': actitud_escuela,
+    #vistas para prácticas
+    'que-haria-en-caso': practica_situacion,
+    'para-prevenir': practica_prevenir,
+    'participacion': practica_participa_prevenir,
+    'como-participacion': practica_como,
+    #vistas para estado actual
+    'problema-comunidad': estado_problema,
+    'problema-pais': estado_problema_pais,
+    'atencion': estado_atencion,
+    'lugares-atencion': estado_lugares,
+    'tipo-atencion': estado_tipo_atencion,
+    'donde-ir': estado_donde,
+    #vistas para percepcion
+    'ninos-abusados': percepcion_ninos_abusados,
+    'donde-se-da':  percepcion_familia,
+    'encargados-prevenir': percepcion_prevenir,
+    'mensajes-ninos': percepcion_mensajes,
+    'poblacion-organizada': percepcion_reducir,
+    'rol-medios': percepcion_medios,
+    'rol-iglesia': percepcion_iglesia,
+    'rol-estado': percepcion_estado,
+    'rol-ong': percepcion_ong,
+    'rol-emp': percepcion_empresa,
+    
+    
     }
 
 def get_prom(total, cantidad):
